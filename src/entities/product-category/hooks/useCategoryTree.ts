c84b082cbb7b3f9c63f72d3ada8acf9ff58566a3
yaron96 <yaronyarik@gmail.com>
2022-09-14 18:09:16 +0300

    fix category tree

--- src/entities/product-category/hooks/useCategoryTree.ts
+++ src/entities/product-category/hooks/useCategoryTree.ts
@@ -5,56 +5,54 @@ import { productCategoryKeys } from "entities/product-category/query-keys";
 import { Key } from "antd/lib/table/interface";
 
 export const useCategoryTree = () => {
-    const [expandedKeys, setExpandedKeys] = useState<Key[]>([]);
+  const [expandedKeys, setExpandedKeys] = useState<Key[]>([]);
 
-    const initialData = {}
-    const { data, isLoading, isError } = useQuery(
-        productCategoryKeys.tree,
-        () => productCategoryApi.getTree(),
-        {
-            initialData: initialData,
-            onSuccess: initExpandedKeys,
-        }
-    );
-
-    function onExpand(_: any, item: any) {
-        const nodeKey = item?.node?.key;
-        if(item.nativeEvent.type !== "dragenter") {
-            if (item.node.expanded) {
-                setExpandedKeys((arr) => arr.filter((e) => e !== nodeKey));
-            } else {
-                setExpandedKeys((arr) => [...arr, nodeKey]);
-            }
-        }
+  const { data, isLoading, isError } = useQuery(
+    productCategoryKeys.tree,
+    () => productCategoryApi.getTree(),
+    {
+      onSuccess: initExpandedKeys,
     }
+  );
 
-    function initExpandedKeys(data: any) {
-        const allKeys = getAllKeys(data);
-        setExpandedKeys((arr: any) => {
-            if (arr.length) return arr;
-            else return allKeys;
-        });
+  function onExpand(_: any, item: any) {
+    const nodeKey = item?.node?.key;
+    if (item.nativeEvent.type !== "dragenter") {
+      if (item.node.expanded) {
+        setExpandedKeys((arr) => arr.filter((e) => e !== nodeKey));
+      } else {
+        setExpandedKeys((arr) => [...arr, nodeKey]);
+      }
     }
+  }
 
-    return {
-        data: data ? data.children : initialData,
-        isError,
-        isLoading,
-        expandedKeys,
-        onExpand,
-    };
+  function initExpandedKeys(data: any) {
+    const allKeys = getAllKeys({ children: data });
+    setExpandedKeys((arr: any) => {
+      if (arr.length) return arr;
+      else return allKeys;
+    });
+  }
+
+  return {
+    data,
+    isError,
+    isLoading,
+    expandedKeys,
+    onExpand,
+  };
 };
 
 const getAllKeys = (node: any) => {
-    const result = [];
-    result.push(node._id);
+  const result = [];
+  result.push(node._id);
 
-    if (node.children.length) {
-        const childKeys = node.children.map((child: any) => getAllKeys(child));
-        childKeys.map((el: any) => {
-            result.push(...el);
-        });
-    }
+  if (node.children.length) {
+    const childKeys = node.children.map((child: any) => getAllKeys(child));
+    childKeys.map((el: any) => {
+      result.push(...el);
+    });
+  }
 
-    return result;
+  return result;
 };
--- src/entities/product/ui/product-filter/category/index.tsx
+++ src/entities/product/ui/product-filter/category/index.tsx
@@ -2,11 +2,16 @@ import { Tree } from "antd";
 import { useCategoryTree } from "entities/product-category/hooks/useCategoryTree";
 
 interface Props {
+  category: string[] | undefined;
   setCategory: (v: string[]) => void;
 }
 
-export const CategoryFilter: React.FC<Props> = ({ setCategory }) => {
+export const CategoryFilter: React.FC<Props> = ({ category, setCategory }) => {
   const { data, expandedKeys, onExpand } = useCategoryTree();
+
+  if (!data) {
+    return null;
+  }
 
   return (
     <Tree
@@ -20,6 +25,7 @@ export const CategoryFilter: React.FC<Props> = ({ setCategory }) => {
       onExpand={onExpand}
       treeData={data}
       expandedKeys={expandedKeys}
+      checkedKeys={category}
       fieldNames={{ key: "_id" }}
     />
   );
